cdc9114c1528d0d483a212fbf1f78147049e5a7a
0xFireball <0xFireball@outlook.com>
2017-08-10 12:40:09 -0700

    show error if config is missing

--- src/mailinglist.cr
+++ src/mailinglist.cr
@@ -2,14 +2,17 @@ require "./config"
 require "./mailinglist/*"
 require "kemal"
 
-# config file
-CONF_FILE = File.read("./mailinglist.config.json")
-
 module MailingList
   puts "MailingList - v#{VERSION}"
 
   # configure
-  CONFIG = MailingListConfig.from_json(CONF_FILE)
+  CONF_FILE = "./mailinglist.config.json"
+  if !(File.exists?(CONF_FILE))
+    puts "Config file #{CONF_FILE} not found. Please refer to the sample config file."
+    exit 1
+  end
+  CONFIG = MailingListConfig.from_json(File.read(CONF_FILE))
+
   LISTMGR = ListManager.new(CONFIG.database.host, CONFIG.database.port, "mailinglist")
 
   Kemal.run CONFIG.server.port
