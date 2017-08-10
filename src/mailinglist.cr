a7ace96e5c25b0c8a0ffe99d4ca6cb591e7cbc13
0xFireball <0xFireball@outlook.com>
2017-08-10 11:09:11 -0700

    add css and stuff

--- src/mailinglist.cr
+++ src/mailinglist.cr
@@ -1,8 +1,11 @@
 require "./mailinglist/*"
 require "kemal"
 
+default_port = 5020
+port = ENV.has_key?("PORT") ? ENV.fetch("PORT").as?(Int32) : default_port
+
 module MailingList
   puts "MailingList - v#{VERSION}"
 
-  Kemal.run
+  Kemal.run port
 end
